a2da7a9bee836692723178b10fd4cb1d21eb6a47
mauricioliradev <mauricioliraweb@gmail.com>
2021-05-03 21:26:10 -0300

    project created

---
+++ backend/src/main/java/com/mlira/mlvendas/MlvendasApplication.java
@@ -0,0 +1,13 @@
+package com.mlira.mlvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlvendasApplication.class, args);
+	}
+
+}
